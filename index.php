da5b73b659dd43ec8c8d1dea2da7d305b401b29b
Denes Csesznegi <denes.csesznegi@codecool.com>
2020-09-23 09:05:15 +0200

    chore: add index php file to render the todo list

---
+++ index.php
@@ -0,0 +1,17 @@
+<?
+
+require_once(dirname(__FILE__) . "/database/connect.php");
+require_once(dirname(__FILE__) . "/classes/todo_list.class.php");
+
+if (isset($_GET["limit"])) {
+    $todoList = new TodoList($_GET["limit"]);
+}
+else {
+    $todoList = new TodoList();
+}
+
+$todoList->render();
+
+require_once("./database/close.php");
+
+?>
